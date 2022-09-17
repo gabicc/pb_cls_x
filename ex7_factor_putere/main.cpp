a4e798c7c617533486ba24f124c96344f6775ee1
gabicc <65864258+gabicc@users.noreply.github.com>
2022-09-17 21:14:22 +0300

    problema 7

---
+++ ex7_factor_putere/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+/*
+Scrieti o functie factor_ putere care primeste ca parametru un numar natural n si care retuneaza
+factorul care apare la puterea cea mai mare in descompunerea lui n in factori primi; daca sunt
+mai multi factori se va returna cel mai mic.
+Folosind apeluri ale functie factor_ putere sa se afiseze factorul acre apare la puterea cea mai
+mare pentru toate numerele din fisierul date.in
+*/
+int main()
+{
+
+    return 0;
+}
